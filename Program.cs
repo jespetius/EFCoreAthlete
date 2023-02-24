1c1b84607d4b68b44679a4d677a26f39c9ee719a
jesperi.kuula <jeppe.kuula@gmail.com>
2023-02-24 15:03:47 +0200

    feat: add single athlete via context

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using AthleteEntity.Data;
+using AthleteEntity.Data.Models;
+
 namespace AthleteEntity
 {
 
@@ -8,6 +11,15 @@ namespace AthleteEntity
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Athlete athobj = new Athlete() { FullName ="Usain Bolt", Sport = "Track and Field", CoachId = 1};
+
+            using(SportManagerDbContext context= new SportManagerDbContext())
+            {
+                context.Athletes.Add(athobj);
+                context.SaveChanges();
+            }
+
         }
     }
 }
